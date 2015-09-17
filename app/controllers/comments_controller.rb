db58253b1c555ba0808a1cd4c876f1565324cb88
Urs Gerber <ug.gerber@gmail.com>
2015-09-17 09:03:33 +0200

    Remove unnecessary views and actions, refactor views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,54 +1,23 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
-  before_action :set_article, only: [:index, :new, :create]
-
-  def index
-    @comments = Comment.all
-  end
-
-  def show
-  end
-
-  def new
-    @comment = Comment.new
-  end
-
-  def edit
-  end
+  before_action :set_comment, only: [:destroy]
+  before_action :set_article, only: [:create]
 
   def create
     @comment = @article.comments.create(comment_params)
 
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @article, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    if @comment.save
+      redirect_to @article, notice: 'Comment was successfully created.'
+    else
+      redirect_to @article, alert: 'Errors occured...'
     end
   end
 
   def destroy
     article = @comment.article
+
     @comment.destroy
-    respond_to do |format|
-      format.html { redirect_to article, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+
+    redirect_to article, notice: 'Comment was successfully destroyed.'
   end
 
   private
